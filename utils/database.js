08db1319a1484daf047b10e250bef11e4773384d
Amitai Sopher <amitai@bluecircle.ai>
2024-10-06 22:31:47 +0300

    remove deprecated flag

--- utils/database.js
+++ utils/database.js
@@ -14,7 +14,6 @@ export const connectToDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI, {
             dbName: "share_prompt",
-            useUnifiedTopology: true,
         })
         isConnected = true;
         console.log("MongoDB is connected");
